4cf9355f8cd863ff70c33fe3d8be6125b35ca03d
Eternal-Love-and-Fire <yevgenkotyk@gmail.com>
2024-08-06 01:09:38 +0300

    asdasd

--- src/pages/app/app.tsx
+++ src/pages/app/app.tsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/portfolio/projects",
+        path: "/projects",
         element: <Projects />,
       },
     ],
